b18dc71808a4ef9e83dd3379367d9a0bf8840b15
= <=>
2024-12-12 08:26:23 +0800

    Remove unnecessary libraries

--- lib/pages/plant_collection/pages/reminder_plan.dart
+++ lib/pages/plant_collection/pages/reminder_plan.dart
@@ -3,7 +3,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:intl/intl.dart';
-import 'package:sprout/main.dart';
 import 'package:sprout/pages/plant_collection/components/reminder_dialogue.dart';
 
 import '../components/edit_reminder.dart';
